99053db4d4cf76a7bf21862082b3af6ddf3876ff
Jacob Raihle <jraihle@gmail.com>
2016-09-05 00:22:04 +0200

    test2

--- web.rb
+++ web.rb
@@ -7,11 +7,12 @@ module SlackGoogleBot
       'Slack integration with Google, https://github.com/dblock/slack-google-bot.'
     end
 	get '/test' do
-	  HTTP.get("https://www.googleapis.com/customsearch/v1", params: {
+	  a = HTTP.get("https://www.googleapis.com/customsearch/v1", params: {
         q: 'Bard',
         key: ENV['GOOGLE_API_KEY'],
         cx: ENV['GOOGLE_CSE_ID']
       })
+	  return "Test result: " + a
 	end
   end
 end
