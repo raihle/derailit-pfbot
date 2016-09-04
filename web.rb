94882fcdf0d1bc4365f410dda668af46cdc441ab
Jacob Raihle <jraihle@gmail.com>
2016-09-05 00:20:49 +0200

    test

--- web.rb
+++ web.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
+require 'http'
 
 module SlackGoogleBot
   class Web < Sinatra::Base
     get '/' do
       'Slack integration with Google, https://github.com/dblock/slack-google-bot.'
     end
+	get '/test' do
+	  HTTP.get("https://www.googleapis.com/customsearch/v1", params: {
+        q: 'Bard',
+        key: ENV['GOOGLE_API_KEY'],
+        cx: ENV['GOOGLE_CSE_ID']
+      })
+	end
   end
 end
